95b9c362bdf9d70b79997b200812c27013350f61
slvr.png <128439456+joeunpyeong@users.noreply.github.com>
2024-07-20 15:28:59 +0900

    수정사항
    우측 상단 검색바 가운데 배치,
    찜목록과  로그인/my page에 검색바 안보이도록 수정

--- reactyogodam/src/App.js
+++ reactyogodam/src/App.js
@@ -1,8 +1,9 @@
+import React from 'react';
+import { Routes, Route, BrowserRouter, useLocation } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import SearchFunc from './Component/SearchFunc';
 import Home from './Component/Home';
-import { Routes, Route, BrowserRouter } from "react-router-dom";
 import LecipeList from './Component/LecipeList';
 import Footer from './Component/FooterBar';
 import LecipeDetail from './Component/LecipeDetail';
@@ -11,6 +12,9 @@ import Login from './Component/Login';
 import JoinUser from './Component/JoinUser';
 
 function App() {
+  const location = useLocation();
+  const showSearchBar = !['/login', '/favorites'].includes(location.pathname);
+
   return (
     <div className="main-container">
       <div className='main-background'>
@@ -18,17 +22,19 @@ function App() {
           <SearchFunc />
         </div>
         <div className="right-section">
-          <div className='right-top-sf'>
-            <SearchFunc></SearchFunc>
-          </div>
+          {showSearchBar && (
+            <div className='right-top-sf'>
+              <SearchFunc />
+            </div>
+          )}
           <div className="content-wrapper">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/lecipeList" element={<LecipeList />} />
               <Route path="/lecipeDetail/:id" element={<LecipeDetail />} />
               <Route path='/lecipeWrite' element={<LecipeWrite />} />
-              <Route path="/login" element={<Login/>} />
-              <Route path='/joinuser' element={<JoinUser/>} />
+              <Route path="/login" element={<Login />} />
+              <Route path='/joinuser' element={<JoinUser />} />
             </Routes>
           </div>
           <Footer />
