32024d4df0807a68d00078d17bab4b83a229dd35
slvr.png <128439456+joeunpyeong@users.noreply.github.com>
2024-07-22 15:18:01 +0900

    .

--- reactyogodam/src/Component/Login.jsx
+++ reactyogodam/src/Component/Login.jsx
@@ -1,12 +1,11 @@
 import axios from '../axios';
 import React, { useState } from 'react';
 import '../css/login.css'; // CSS 파일 경로 확인 필요
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     const [user_id, setUser_id] = useState('');
     const [user_pw, setUser_pw] = useState('');
-    const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -16,44 +15,40 @@ const Login = () => {
                 user_pw : user_pw,
             });
             console.log(response);
-
-            console.log(response);
-            localStorage.setItem('token', response.data.token);
-            navigate('/');
+            // console.log("tleh " + response.data.token);
+            // // 토큰을 로컬 스토리지에 저장
+            //localStorage.setItem('token', response.data.token);
         } catch (error) {
             console.error('Login failed:', error);
         }
     };
 
     return (
-        <div class="container">
-        <header>
-            <a href="#" class="back-button"></a>
-        </header>
-        <main>
-            <a class="bannerImg"></a>
-            <form>
-                <div class="input-group">
-                    <label for="email">이메일을 입력해 주세요</label>
-                    <input type="email" id="email" placeholder="이메일을 입력해 주세요"/>
+        <div className='login_container'>
+
+            <form onSubmit={handleSubmit} method='post'>
+                <div className='btn_container'>
+                <button type="button">◀</button>
                 </div>
-                <div class="input-group">
-                    <label for="password">비밀번호를 입력해 주세요</label>
-                    <input
+                <h2>요리 고민 상담</h2>
+                <h1>요고담</h1>
+                <input
+                    type="text"
+                    placeholder="user_id"
+                    onChange={(e) => setUser_id(e.target.value)}
+                    required
+                />
+                <input
                     type="password"
                     placeholder="Password"
                     onChange={(e) => setUser_pw(e.target.value)}
-                    required/>
-                </div>
-                <button type="submit" class="login-button">로그인</button>
+                    required
+                />
+                <button type="submit">로그인</button>
+                <p>아직 회원이 아니신가요? <Link to='/joinuser'>회원가입</Link></p>
             </form>
-            <p class="signup-text">아직 회원이 아니신가요?<Link to='/joinuser'>회원가입</Link></p>
-        </main>
-    </div>
-
+        </div>
     );
 };
 
 export default Login;
-
-
