469f557413c458d7edb119767a2711083acd49b0
DOMADO <dhrrp321@naver.com>
2024-07-23 18:00:32 +0900

    feat : 롤백 ※ feat : 홈화면 DB 데이터 연동 커밋 취소
    - Merge branch 'jep' 커밋 상태로 변경

--- reactyogodam/src/Component/Home.jsx
+++ reactyogodam/src/Component/Home.jsx
@@ -1,34 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 import BannerSlide from './BannerSlide';
 import '../css/home.css';
 
 const Home = () => {
+
     const images = ['/img/testImg/groundImg.png', '/img/testImg/totoro1.png', '/img/testImg/test1.png'];
+    //const images = ['img1.png', 'img2.png', 'img1.png'];
     const intervalTime = 3000; // 3초
 
     const [cardInfoList, setCardInfoList] = useState([]);
     const [activeTab, setActiveTab] = useState('popular'); // 현재 활성화된 탭
 
     const navigate = useNavigate();
+    
+    const products = [
+        { id: 1, name: "상품1", img:'/img/testImg/groundImg.png', price: 1000, content: "상품1입니다" },
+        { id: 2, name: "상품2", img:'/img/testImg/totoro1.png', price: 2000, content: "상품2입니다" },
+        { id: 3, name: "상품3", img:'/img/testImg/test1.png', price: 3000, content: "상품3입니다" },
+        { id: 4, name: "상품4", img:'/img/testImg/groundImg.png', price: 3000, content: "상품3입니다" },
+        { id: 5, name: "상품5", img:'/img/testImg/totoro1.png', price: 3000, content: "상품3입니다" },
+        { id: 6, name: "상품6", img:'/img/testImg/test1.png', price: 3000, content: "상품3입니다" }
+    ];
 
     useEffect(() => {
-        axios.get('http://localhost:8000/list/products')
-            .then(response => {
-                const products = response.data.map(item => ({
-                    id: item.FOOD_ID,
-                    name: item.FOOD_NAME,
-                    img: `data:image/jpeg;base64,${item.FOOD_IMG}`,
-                    price: item.FOOD_PRICE,
-                    content: item.CUSINE_TYPE,
-                }));
-                setCardInfoList(products);
-            })
-            .catch(error => {
-                console.error('There was an error fetching the data!', error);
-            });
-    }, []);
+        setCardInfoList(products);
+    }, [])
 
     const handleProductClick = (id) => {
         navigate(`/lecipeDetail/${id}`);
@@ -51,19 +49,37 @@ const Home = () => {
             </div>
             <div>
                 {cardInfoList.map((product, index) => (
-                    <div className="product-card" key={product.id} onClick={() => handleProductClick(product.id)}>
-                        <div className="product-rank">{index + 1}</div>
+                    <div className="product-card" onClick={() => handleProductClick(product.id)}>
+                         <div className="product-rank">{index + 1}</div>
                         <img className="product-img" src={product.img} alt={product.name} />
                         <div className="product-info">
-                            <h2>{product.name}</h2>
-                            <p>{product.content}</p>
-                            <p className="product-price">{product.price.toLocaleString()} 원</p>
+                            <div>
+                                <h2>{product.name}</h2>
+                                <img className='bookmark_icon' src="../img/icon/bookmark.svg" alt="bookmark" />
+                            </div>
+
+                            <div>
+                                <div>
+                                    <div>
+                                        <div >
+                                            <img className="user_icon" src="../img/icon/User.svg" alt="user"/><p>{product.content}</p>
+                                        </div>
+                                        <div>
+                                            <img className='star_icon' src="../img/icon/star.svg" alt="star"/>
+                                            <p>5.0</p>
+                                            <br />
+                                        <img className='search_icon' src="../img/icon/search.svg" alt="search"/><p>2964</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <p className="product-price">{product.price.toLocaleString()} 원</p>
+                            </div>
                         </div>
                     </div>
                 ))}
             </div>
         </div>
-    );
+);
 }
 
-export default Home;
+export default Home;    
