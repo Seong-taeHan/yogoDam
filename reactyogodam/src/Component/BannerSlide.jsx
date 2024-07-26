2247c457b550ef9244fd736c4aa41bf68dad37b6
Seong-taeHan <kmljjn22@gmail.com>
2024-07-26 09:43:08 +0900

    feat : 배너 슬라이드 수정 - 이미지 밀리는 오류 수정

--- reactyogodam/src/Component/BannerSlide.jsx
+++ reactyogodam/src/Component/BannerSlide.jsx
@@ -1,39 +1,40 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import '../css/bannerSlide.css';
 
 const BannerSlide = ({ images, intervalTime }) => {
     const [currentIdx, setCurrentIdx] = useState(0);
-  
-    useEffect(() => {
-      const intervalId = setInterval(() => {
-        setCurrentIdx((prevIdx) => (prevIdx + 1) % images.length);
-      }, intervalTime);
-  
-      return () => clearInterval(intervalId);
-    }, [images.length, intervalTime]);
-  
-    return (
-      <div className="banner-container">
-        <div
-          className="banner-slide"
-          style={{ transform: `translateX(-${currentIdx * 100}%)` }}
-        >
-          {images.map((image, index) => (
-            <img src={image} alt={`Slide ${index}`} key={index} />
-          ))}
-        </div>
-        <div className="dots">
-          {images.map((_, index) => (
-            <div
-              key={index}
-              className={`dot ${index === currentIdx ? 'active' : ''}`}
-              onClick={() => setCurrentIdx(index)}
-            ></div>
-          ))}
-        </div>
-      </div>
-    );
-  };
 
-export default BannerSlide
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentIdx((prevIdx) => (prevIdx + 1) % images.length);
+        }, intervalTime);
+
+        return () => clearInterval(intervalId);
+    }, [images.length, intervalTime]);
+
+    return (
+        <div className="banner-container">
+            <div
+                className="banner-slide"
+                style={{ transform: `translateX(-${currentIdx * 100}%)` }}
+            >
+                {images.map((image, index) => (
+                    <div className="banner-slide-item" key={index}>
+                        <img src={image} alt={`Slide ${index}`} />
+                    </div>
+                ))}
+            </div>
+            <div className="dots">
+                {images.map((_, index) => (
+                    <div
+                        key={index}
+                        className={`dot ${index === currentIdx ? 'active' : ''}`}
+                        onClick={() => setCurrentIdx(index)}
+                    ></div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default BannerSlide;
