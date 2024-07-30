825c925a7b37d50cf378584da7d51faba9e97e14
Seong-taeHan <kmljjn22@gmail.com>
2024-07-30 21:31:26 +0900

    feat : aiselect 페이지 수정 - 화면 구성 수정

--- reactyogodam/src/Component/AiSelectPage.jsx
+++ reactyogodam/src/Component/AiSelectPage.jsx
@@ -39,34 +39,32 @@ const AiSelectPage = () => {
     };
 
     return (
-        <div>
-            <div className="aiselect-container">
-                <div className="aiselect-TopBar">추천 AI</div>
-                <div className="aiselect-Content">
-                    <div className="aiselect-Text">여러 요리를 추천해 드릴게요!</div>
-                    <Swiper
-                        className="aiselect-swiper"
-                        modules={[Navigation, Pagination]}
-                        spaceBetween={50}
-                        slidesPerView={1}
-                        navigation
-                        pagination={{ clickable: true }}
-                        loop={loop}
-                    >
-                        {slides.map((slide, index) => (
-                            <SwiperSlide key={index} onClick={() => handleSlideClick(slide)}>
-                                <div className="aiselect-slide-content">
-                                    <img src={slide.img} alt={slide.text} className="aiselect-slide-image" />
-                                    <div className="aiselect-slide-text">{slide.text}</div>
-                                </div>
-                            </SwiperSlide>
-                        ))}
-                    </Swiper>
-                </div>
-                <div>
-                    <div className='aiselect-sbtitle-align'>
-                        <img src="/img/aicategory/aiconhalf.png" alt="AI 추천" className="aiselect-Image" />
-                    </div>
+        <div className="aiselect-container">
+            <div className="aiselect-TopBar">추천 AI</div>
+            <div className="aiselect-Content">
+                <div className="aiselect-Text">여러 요리를 추천해 드릴게요!</div>
+                <Swiper
+                    className="aiselect-swiper"
+                    modules={[Navigation, Pagination]}
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    navigation
+                    pagination={{ clickable: true }}
+                    loop={loop}
+                >
+                    {slides.map((slide, index) => (
+                        <SwiperSlide key={index} onClick={() => handleSlideClick(slide)}>
+                            <div className="aiselect-slide-content">
+                                <img src={slide.img} alt={slide.text} className="aiselect-slide-image" />
+                                <div className="aiselect-slide-text">{slide.text}</div>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
+            <div className="aiselect-footer">
+                <div className='aiselect-sbtitle-align'>
+                    <img src="/img/aicategory/aiconhalf.png" alt="AI 추천" className="aiselect-Image" />
                 </div>
             </div>
         </div>
